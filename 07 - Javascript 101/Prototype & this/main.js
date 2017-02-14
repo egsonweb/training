580d02c89c10fd8d7b0806eca3d247b5355898b8
whoisandie <rjn143@gmail.com>
2017-02-14 00:16:37 -0800

    add prototype and inheritance example

--- 07 - Javascript 101/Prototype & this/main.js
+++ 07 - Javascript 101/Prototype & this/main.js
@@ -1,91 +1,29 @@
-// print()
-// var a = 10;
-// function print() {
-//   console.log(a)
-// }
-// // this => window
-// function fn1() {
-//   // this => fn1
-//   var c;
-//   c = "Hello there!";
-//   fn2();
-//   console.log('From outer: ' + a)
-//   console.log('From outer: ' + c)
-//   function fn2() {
-//     var c = "Some other string"
-//     console.log('From inner fn2: ' + a)
-//     console.log('From inner fn2: ' + c)
-//     function fn3() {
-//       console.log('From inner fn3: ' + c)
-//     }
-//     fn3();
-//   }
-// }
-//
-// var b = 20;
-//
-// fn1()
-
-
-// var person = {
-//   name: 'John',
-//   age: 42,
-//   friends: ['Doe', 'Mike', 'Jake'],
-//   address: {
-//     city: 'Newark',
-//     state: 'CA'
-//   }
-// };
-
-
-//
-// console.log(person)
-// console.log(person.name)
-// console.log(person.address.city)
-
-// for(var i=0; i<=10; i++) {
-//   console.log(i);
-// }
-
-// for(var i=0; i<=10; i++) {
-//   (function(x) {
-//     setTimeout(function() {
-//       console.log(x);
-//     }, 2000*x);
-//   })(i);
-// }
-
-var startBtn = document.getElementById('start')
-var stopBtn = document.getElementById('stop')
-var resetBtn = document.getElementById('reset')
-var number = document.getElementById('number')
-
-var count = 0;
-var interval = null;
-
-function stop() {
-  if (interval && interval !== null) {
-    clearInterval(interval);
-  }
-}
-
-function start() {
-  interval = setInterval(function() {
-    count++;
-    (function(x) {
-      if(x === 5) {
-        clearInterval(interval)
+(function(w) {
+  var test = (function() {
+    var name = "John"
+    var person = {
+      name: 'Jake',
+      eat: function(n) {
+        console.log(n + ' is eating')
+      },
+      move: function() {
+        foo()
+        return name + ' is moving...'
       }
-      number.innerHTML = count;
-      console.log(x)
-    })(count)
-  }, 1000);
-}
+    };
 
-function reset() {
-  count = 0;
-  number.innerHTML = count;
-}
+    function foo() {
+      console.log('Returning something...')
+    }
 
-// startBtn.onclick = start;
-// stopBtn.onclick = stop;
+    return {
+      hello: person.move,
+      eat: person.eat
+    };
+  })();
+
+  w.main = {
+    sayHello: test.hello,
+    eatSomething: test.eat.bind(null, "Mike")
+  }
+})(window)
