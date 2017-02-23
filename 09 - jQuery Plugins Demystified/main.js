580a3a513cf7fb022d36f65a7bd5f1548ba8cbc8
whoisandie <rjn143@gmail.com>
2017-02-22 19:13:48 -0800

    move files to respective locations

--- 09 - jQuery Plugins Demystified/main.js
+++ 09 - jQuery Plugins Demystified/main.js
@@ -1,31 +1,31 @@
-$(document).ready(function() {
-  var $prev = $('a.prev');
-  var $next = $('a.next');
-  var current = 0;
-  var length = $('.slider-item').length;
+$(document).ready(function () {
+    var $prev = $('a.prev');
+    var $next = $('a.next');
+    var current = 0;
+    var length = $('.slider-item').length;
 
-  $prev.on('click', function(e) {
-    e.preventDefault();
-    current -= 1;
-    if (current <= 0) {
-      current = length - 1;
-    }
-    $('.slider').css({
-      marginLeft: current*-820 + 'px'
-    })
-  });
+    $prev.on('click', function (e) {
+        e.preventDefault();
+        current = current - 1;
+        if (current <= 0) {
+            current = length - 1;
+        }
+        $('.slider').css({
+            marginLeft: current * -820 + 'px'
+        })
+    });
 
-  $next.on('click', function(e) {
-    e.preventDefault();
-    current += 1;
-    if (current >= length) {
-      current = current % length;
-    }
-    $('.slider').css({
-      marginLeft: current*-820 + 'px'
-    })
-  });
+    $next.on('click', function (e) {
+        e.preventDefault();
+        current += 1;
+        if (current >= length) {
+            current = current % length;
+        }
+        $('.slider').css({
+            marginLeft: current * -820 + 'px'
+        });
+    });
 
-  $('.slider-item > p:nth-child(1), .slider-item > p:nth-child(2)').truncate()
+    $('.slider-item > p:nth-child(1), .slider-item > p:nth-child(2)').truncate();
 
-})
+});
